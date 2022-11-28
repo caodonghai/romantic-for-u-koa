f149b4baf3fcde18deb1aee48e9f74bd045ab0d4
Cdh731700 <caodonghai234@163.com>
2022-11-28 11:55:51 +0000

    feat: dayin

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,6 @@
-const home = require('./home');
-const count = require('./count');
-const wishList = require('./wishList');
-const user = require('./user');
+const home = require("./home");
+const count = require("./count");
+const wishList = require("./wishList");
+const user = require("./user");
 
-module.exports = [home, count, wishList, user];
+module.exports = [home, count];
