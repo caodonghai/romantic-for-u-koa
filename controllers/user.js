a95d38e13aae5b71feee3ad070732304bf2bf479
Cdh731700 <caodonghai234@163.com>
2022-11-28 11:27:04 +0000

    feat: 添加日志

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,10 @@ const { User } = require("../db");
  * 获取文件
  */
 exports.getUserList = async (ctx) => {
+  console.log({ctx})
   const result = await User.findAndCountAll();
+
+  console.log({ctx, result})
 
   ctx.body = {
     code: 200,
