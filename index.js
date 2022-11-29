c39c266e6f16b592fa1e7d091991bbdbefec4b23
Cdh731700 <caodonghai234@163.com>
2022-11-29 02:01:48 +0000

    feat: 添加打印日志

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const bodyParser = require("koa-bodyparser");
 const { init: initDB } = require("./db");
 const koaRequestProxy = require("./middleware/koaRequestProxy");
 const routers = require("./routers/index");
+const count = require("./routers/count");
 
-console.log({ routers });
+console.log({ routers, count });
 
 const app = new Koa();
 app
   .use(logger())
   .use(bodyParser())
-  .use(...routers.map((item) => item.routes()))
-  .use(...routers.map((item) => item.allowedMethods()))
+  .use(count.routes())
+  // .use(...routers.map((item) => item.allowedMethods()))
   .use(koaRequestProxy());
 
 const port = process.env.PORT || 80;
