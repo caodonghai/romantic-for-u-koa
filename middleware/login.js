0b51caec531885f6af9fc8e1962b657b70f88267
Cdh731700 <caodonghai234@163.com>
2022-12-30 07:10:32 +0000

    feat: 登陆权限

--- middleware/login.js
+++ middleware/login.js
@@ -5,15 +5,21 @@ module.exports = () => {
     // 使用
     let { url, header } = ctx;
     const {userName, openId} = header;
+    console.log({userName, openId, ctx})
     if(!userName) {
         ctx.body = {
             code: 302,
             Locatioin: '/pages/login/index'
         };
-    } else {
-        await isLoginWithUserName(userName, ctx)
+        return
+    } else if(!isLoginWithUserName(userName, ctx)) {
+        ctx.body = {
+            code: 502,
+            errMsg: "用户不存在",
+            data: {},
+        };
+        return
     }
-
     return await next();
   };
 };
@@ -26,12 +32,8 @@ async function isLoginWithUserName(userName, ctx) {
       userName: decodeUserName,
     },
   });
-
-  if (!result || !Object.keys(result).length) {
-    ctx.body = {
-      code: 502,
-      errMsg: "用户不存在",
-      data: {},
-    };
+  if (result && Object.keys(result).length) {
+      return true
   }
+  return false
 };
