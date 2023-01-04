f837c38996f0ad7d5bdd4f9905da8f52fabe42a2
Cdh731700 <caodonghai234@163.com>
2023-01-04 07:31:14 +0000

    feat: 精简路由

--- routers/countdownDays.js
+++ routers/countdownDays.js
@@ -4,21 +4,21 @@ const controllers = require("../controllers/countdownDays");
 const router = new Router();
 
 // 获取列表
-router.get("/api/wish/countdownDaysList", controllers.countdownDaysList);
+router.get("/api/countdownDays/list", controllers.countdownDaysList);
 
 // 详情
-router.get("/api/wish/:id/countdownDaysById", controllers.countdownDaysById);
+router.get("/api/countdownDays/:id/detail", controllers.countdownDaysById);
 
 // 用户下的数据列表
-router.get("/api/wish/countdownDaysByUserName", controllers.countdownDaysByUserName);
+router.get("/api/countdownDays/listByUserName", controllers.countdownDaysByUserName);
 
 // 创建
-router.post("/api/wish/createCountdownDay", controllers.createCountdownDay);
+router.post("/api/countdownDays/create", controllers.createCountdownDay);
 
 // 更新
-router.post("/api/wish/:id/updateCountdownDay", controllers.updateCountdownDay);
+router.post("/api/countdownDays/:id/update", controllers.updateCountdownDay);
 
 // 删除
-router.post("/api/wish/:id/deleteCountdownDay", controllers.deleteCountdownDay);
+router.post("/api/countdownDays/:id/delete", controllers.deleteCountdownDay);
 
 module.exports = router;
